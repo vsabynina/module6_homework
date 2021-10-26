5d50cc5463aba9d5e586246dfa2511e6c23c132e
Your Name <vika.sabynina@mail.ru>
2021-10-26 14:50:07 +0300

    deleted comment in exercise_4.js

--- exercise_4.js
+++ exercise_4.js
@@ -1,13 +1,3 @@
-/* function getNumbers (a, b) {
-  const intervalNum = setInterval(function(){
-    if (a === b) {
-      clearInterval(intervalNum);
-    }
-    a++;
-    console.log(a);
-  }, 1000);
-}
-getNumbers(5, 15); */
 function getNumbers (a, b) {
   const intervalNum = setInterval(function(){
     console.log(a);
