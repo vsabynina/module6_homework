500c1895bdfe23a7ebc0ebff9d87ef3435c66c39
Your Name <vika.sabynina@mail.ru>
2021-10-26 14:45:07 +0300

    fixed exercise_4.js

--- exercise_4.js
+++ exercise_4.js
@@ -1,12 +1,21 @@
-function getNumbers (a, b) {
+/* function getNumbers (a, b) {
   const intervalNum = setInterval(function(){
     if (a === b) {
       clearInterval(intervalNum);
-    } else {
-      for (a; a  <= b; a++) {
-        console.log(a);
-      }
     }
+    a++;
+    console.log(a);
   }, 1000);
 }
+getNumbers(5, 15); */
+function getNumbers (a, b) {
+  const intervalNum = setInterval(function(){
+    console.log(a);
+    if (a === b) {
+      clearInterval(intervalNum);
+    } else if (a <= b) {
+      a++;
+    }
+  }, 1000);
+  }
 getNumbers(5, 15);
